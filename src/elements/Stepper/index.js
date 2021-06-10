73d562affa01fc509ee97a694ddb31b0815188e1
unknown <alvinsanudharma@gmail.com>
2021-06-10 10:57:36 +0800

    Component Stepper

---
+++ src/elements/Stepper/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import propTypes from "prop-types";
+
+export default function Stepper(props) {
+  const { steps, initialStep } = props;
+  const stepKeys = Object.keys(steps);
+
+  const [currentStep, setCurrentStep] = useState(
+    stepKeys.indexOf(initialStep) ? initialStep : stepKeys[0]
+  );
+
+  const totalStep = stepKeys.length;
+  const indexStep = stepKeys.indexOf(currentStep);
+
+  function prevStep() {
+    if (+indexStep > 0) {
+      setCurrentStep(stepKeys[indexStep - 1]);
+    }
+  }
+
+  function nextStep() {
+    if (+indexStep < totalStep) {
+      setCurrentStep(stepKeys[indexStep + 1]);
+    }
+  }
+
+  return <>{props.children(prevStep, nextStep, currentStep, steps)}</>;
+}
+
+Stepper.propTypes = {
+  data: propTypes.object,
+  initialStep: propTypes.string,
+};
